09ad5a68121810a558a43ce1ce44b1e205191eab
Vuong Bui <vuongbui@Vuongs-MacBook-Pro.local>
2019-04-26 16:10:42 +0700

    Add modal and home page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('add_event.html')
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.run(debug=True)
--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 app = Flask(__name__)
 
@@ -27,3 +29,25 @@ app.register_blueprint(events_blueprint, url_prefix="/events")
 # app.register_blueprint(users_blueprint, url_prefix="/users")
 
 # app.register_blueprint(users_blueprint, url_prefix="/auth")
+
+POSTGRES = {
+    'user': os.environ['PSQL_USER'],
+    'pw': os.environ['PSQL_PWD'],
+    'db': os.environ['PSQL_DB'],
+    'host': os.environ['PSQL_HOST'],
+    'port': os.environ['PSQL_PORT'],
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:\
+%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
