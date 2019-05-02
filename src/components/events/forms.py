f5bd1767cb04848931d786b0f223656294567e28
Vuong Bui <vuongbui@Vuongs-MacBook-Pro.local>
2019-05-02 15:31:05 +0700

    Add signup page

--- src/components/events/forms.py
+++ src/components/events/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, DateTimeField, validators, SelectField 
+from wtforms import StringField, IntegerField, SubmitField, DateTimeField, validators, SelectField ,PasswordField
 
 class AddEventForm(FlaskForm):
     description = StringField('Description of Event:', [validators.Length(min=4, max=25)])
@@ -9,6 +9,18 @@ class AddEventForm(FlaskForm):
     start_date = StringField('Start date of Event:', [validators.Length(min=4, max=25)])
     end_date = StringField('End date of Event:', [validators.Length(min=4, max=25)])
 
+class SignUpForm(FlaskForm):
+    username=StringField('Username: ',[validators.DataRequired(),validators.Length(min=6,max=16)])
+    email=StringField("Email: ",[validators.DataRequired()])
+    # phonenumber=StringField("Phone Number: ")
+    password_hash=StringField("Password: ",[validators.DataRequired()])
+    # starttime=DateField("Time", format='%Y-%m-%d')
+    submit=SubmitField('Create' )
 
+class LoginForm(FlaskForm):
+    email=StringField("Email: ",[validators.DataRequired()])
+    password_hash=PasswordField("Password: ",[validators.DataRequired()])
+    # starttime=DateField("Time", format='%Y-%m-%d')
+    submit=SubmitField('Login' )
 
 
--- src/components/events/views.py
+++ src/components/events/views.py
@@ -1,11 +1,18 @@
 from flask import Blueprint, render_template
+from flask import Flask, render_template, flash, redirect, url_for,request
+
+from flask_login import UserMixin, LoginManager,login_user,logout_user, current_user, login_required
+from flask import request
+from flask import session
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 events_blueprint = Blueprint('event',
     __name__,
     template_folder='../../templates/events')
 
 
-from src.components.events.forms import AddEventForm
+from src.components.events.forms import AddEventForm, SignUpForm, LoginForm
 from src.models.user import User 
 from src.models.event import Event
 from src import db
@@ -21,6 +28,46 @@ def add():
         db.session.commit()
     return render_template("add_event.html", form=form)
 
-@events_blueprint.route('/list')
-def list():
-    return "Here are all the events"
+
+@events_blueprint.route('/signup',methods=['POST','GET'])
+def signup():
+    form=SignUpForm()
+    if request.method == 'POST':
+        u=User(email=form.email.data,password_hash=generate_password_hash(form.password_hash.data))
+        print("username")
+        db.session.add(u)
+        db.session.commit()
+        flash("Your account has been successfully created")
+        print('*******', u.id)
+        return redirect('/')
+    return render_template('signup.html',form=form)  
+
+
+
+@events_blueprint.route('/login',methods=['POST','GET'])
+def login():
+    form=LoginForm()
+    if request.method == 'POST':
+        email=form.email.data
+        password=form.password_hash.data
+        user=User.query.filter_by(email=email).first()
+        if user is not None and check_password_hash(user.password_hash,password):
+            login_user(user)
+            session['username'] = user.username
+            
+ #user here being the user object you have queried
+            print(user.username)
+    return render_template('home.html', form=form)
+
+
+# @events_blueprint.route('/logout')
+# @login_required
+# def logout():
+#     logout_user()
+#     del session['username']
+#     return redirect('/')
+
+
+
+        
+
